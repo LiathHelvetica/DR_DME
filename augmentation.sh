fe674e79b1d4b6621bd154d46320f27125acc929
liath <matthewdragun@gmail.com>
2024-07-28 16:12:20 +0200

    wip

--- augmentation.sh
+++ augmentation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-arr=(224 232)
+arr=(260 272 246 238 288 256 342 230 236 224 232)
 
 for i in "${arr[@]}"
 do
-  mkdir /home/liath/DR_DME_DATA/aug_out_b2_$i
+  mkdir /home/liath/DR_DME_DATA/aug_out_plain_b2_$i
   python augmentation_sh_pipeline.py ${i}
 done
